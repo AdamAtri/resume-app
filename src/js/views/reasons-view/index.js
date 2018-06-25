fb2885eb01166bc9caf6486eb800ffab1f692e98
adam <adam@atricoware.com>
2018-06-25 12:13:17 -0400

    Makes view to state reasons.

---
+++ src/js/views/reasons-view/index.js
@@ -0,0 +1,24 @@
+// ReasonsView
+//   description
+
+module.exports = (function() {
+
+  const template = require('./template');
+  const ReasonsView = Mn.View.extend({
+    template: template,
+    className: 'reasons-view',
+    ui: {},
+    regions: {},
+
+    initialize() {
+
+    },
+    onAttach() {
+
+    },
+
+
+  });
+
+  return ReasonsView;
+})();
---
+++ src/js/views/reasons-view/template.js
@@ -0,0 +1,9 @@
+// LiesView template;
+
+module.exports = () => (`
+  <ul class="reasons-list">
+    <li class="reason">
+      <div>More than 3000 false or misleading claims... that's 6.5+ a day.</div>
+    </li>
+  </ul>
+`);
