5a0a6f357391954cfbc67184ac47aea2ae055828
adam <adam@atricoware.com>
2018-06-27 12:25:51 -0400

    Adds a view to list education.

---
+++ src/js/views/resume-view/education.js
@@ -0,0 +1,37 @@
+// EducationView
+//   Display my education info
+
+module.exports = (function() {
+
+  const template = (data) => {
+    let eds = data.education.map(ed => {
+      return (`
+        <hr>
+        <div class="educator">
+          <h6 class="ed-degree">${ed.degree}</h6>
+          <div class="ed-details">
+            ${ed.school}, ${ed.location}, ${ed.start} - ${ed.end}
+          </div>
+        </div>
+      `);
+    });
+
+    return (`
+      <div class="section-title">Edumacation</div>
+      <div class="section-details education-details">${eds.join('\n')}</div>
+    `);
+  };
+
+  const EducationView = Mn.View.extend({
+    template: template,
+    templateContext() {
+      return this.options;
+    },
+    tagName: 'section',
+    id: 'education',
+    className: 'main-section',
+
+  });
+
+  return EducationView;
+})();
